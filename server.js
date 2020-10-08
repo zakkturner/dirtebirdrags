e358d307d683c55724b801cfeb87876cd93635ee
zakkturner <30674356+zakkturner@users.noreply.github.com>
2020-10-07 23:40:22 -0400

    woocommerce hooked up to react app

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const next = require("next");
 const express = require("express");
+const wooConfig = require("./wooConfig");
 
 const WooCommerceAPI = require("woocommerce-api");
+
+const WooCommerce = new WooCommerceAPI({
+  url: wooConfig.siteUrl,
+  consumerKey: wooConfig.consumerKey,
+  consumerSecret: wooConfig.consumerSecret,
+  wpAPI: true,
+  version: "wc/v1",
+});
 
 const port = 3000;
 const dev = process.env.NODE_ENV !== "production";
@@ -13,9 +22,16 @@ app
   .then(() => {
     const server = express();
 
+    server.get("/getProducts", (req, response) => {
+      WooCommerce.get("products", function (err, data, res) {
+        response.json(JSON.parse(res));
+      });
+    });
+
     server.get("*", (req, res) => {
       return handle(req, res);
     });
+
     server.listen(port, (err) => {
       if (err) {
         throw err;
