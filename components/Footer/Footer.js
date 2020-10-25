3eff8cbbb144c84e811937bf0f58d9e324d755e3
zakkturner <30674356+zakkturner@users.noreply.github.com>
2020-10-24 23:40:08 -0400

    mobile footer done

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,5 +1,32 @@
 import styles from "./Footer.module.scss";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
-  return <footer className={styles.footer}>footer</footer>;
+  return (
+    <>
+      <footer className={styles.footer}>
+        <div className={styles.footer__container}>
+          <ul className={styles.footer__container__links}>
+            <li className={styles.footer__container__links_link}>
+              <Link href="/policy">
+                <a>Policy</a>
+              </Link>
+            </li>
+            <li className={styles.footer__container__links_link}>
+              <Link href="/terms">
+                <a>Terms</a>
+              </Link>
+            </li>
+            <li className={styles.footer__container__links_link}>
+              <Link href="https://www.instagram.com/dirtebirdrags/">
+                <FontAwesomeIcon icon={faInstagram} />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </>
+  );
 }
--- components/Layout.js
+++ components/Layout.js
@@ -14,6 +14,8 @@ export default function Layout(props) {
             <link
               rel="stylesheet"
               href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+              integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+              crossorigin="anonymous"
             />
           </Head>
           <Header />
